f164477d773144f1ae5e46caea9fd23f1f316188
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-04-18 08:53:13 +0100

    Getting jobs list from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { FaAngleDoubleRight } from 'react-icons/fa';
 const url = 'https://course-api.com/react-tabs-project';
 
 import './App.css';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+  // Check api if jobs returned, use React Dev Tools
+  console.log(jobs);
+
+  if (loading) {
+    return (
+      <section className='section loading'>
+        <h3>loading...</h3>
+      </section>
+    );
+  }
   return (
     <div className='App'>
-      <h2>React Tabs App</h2>
+      <h2>jobs</h2>
     </div>
   );
 }
